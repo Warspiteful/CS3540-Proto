39c0ac5c063f511415edeb311f4378c2bf970148
nicolesorial <grace.cola@gmail.com>
2022-10-16 18:46:14 -0400

    screenshot icons

---
+++ Assets/Nicole/Scripts/IconGenerator.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class IconGenerator : MonoBehaviour
+{
+
+    private Camera sceneCamera;
+    [SerializeField] private String pathFolder;
+
+    public List<GameObject> sceneObjects;
+    public List<InventoryItemData> dataObjects;
+
+    private void Awake()
+    {
+        sceneCamera = GetComponent<Camera>();
+    }
+
+    [ContextMenu("Screenshot")]
+    private void ProcessScreenshots()
+    {
+        StartCoroutine(Screenshot());
+    }
+
+    void TakeScreenshot(string fullPath)
+    {
+        if (sceneCamera == null)
+        {
+            sceneCamera = GetComponent<Camera>();
+        }
+
+        RenderTexture rt = new RenderTexture(256, 256, 24);
+        sceneCamera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        sceneCamera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+        sceneCamera.targetTexture = null;
+        RenderTexture.active = null;
+
+        if (Application.isEditor)
+        {
+            DestroyImmediate(rt);
+        }
+        else
+        {
+            Destroy(rt);
+        }
+
+        byte[] bytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(fullPath, bytes);
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+#endif
+    }
+
+    private IEnumerator Screenshot()
+    {
+        for (int i = 0; i < sceneObjects.Count; i++)
+        {
+            GameObject obj = sceneObjects[i];
+            InventoryItemData data = dataObjects[i];
+            
+            obj.gameObject.SetActive(true);
+
+            yield return null;
+            
+            TakeScreenshot($"{Application.dataPath}/{pathFolder}/{data.id}_Icon.png");
+
+            yield return null;
+            obj.gameObject.SetActive(false);
+
+            Sprite s = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/{pathFolder}/{data.id}_Icon.png");
+            if (s != null)
+            {
+                data.icon = s;
+                EditorUtility.SetDirty(data);
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Nicole/Scripts/InventoryItemData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This system was creating using the following tutorial: https://www.youtube.com/watch?v=SGz3sbZkfkg
+[CreateAssetMenu(menuName = "Inventory Item Data")]
+public class InventoryItemData : ScriptableObject
+{
+    public string id;
+    public string displayName;
+    public Sprite icon;
+    public GameObject prefab;
+}
