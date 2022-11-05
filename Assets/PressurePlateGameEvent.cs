281aa2b8936b06fda0f7fc8be0866818777de405
ortiz-da <ortiz.da@northeastern.edu>
2022-11-04 23:58:24 -0400

    most puzzles work

--- Assets/PressurePlateGameEvent.cs
+++ Assets/PressurePlateGameEvent.cs
@@ -1,29 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.ProBuilder.Shapes;
+
 public class PressurePlateGameEvent : MonoBehaviour
 {
-    public GameObject toActivate;
-
-    public bool onByDefault;
-
     private int collisions;
 
+    [SerializeField]
+    private GameObject door;
 
+    private Animator doorAnimator;
     
     private UnityEvent event1;
     private UnityEvent event2;
 
+
     private void Start()
     {
-        toActivate.SetActive(onByDefault);
+        doorAnimator = door.GetComponent<Animator>();
         collisions = 0;
+        https://youtu.be/15n-ilpYqME
+        Gizmos.DrawLine(this.transform.position, doorAnimator.transform.position);
+
     }
+
+    private void Update()
+    {
+        Debug.DrawLine(this.transform.position, door.transform.position, Color.magenta);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        toActivate.SetActive(!onByDefault);
+        if (collisions == 0)
+        {
+            doorAnimator.Play("DoorAnimation");
+        }
         collisions += 1;
     }
 
@@ -32,7 +47,8 @@ public class PressurePlateGameEvent : MonoBehaviour
         collisions -= 1;
         if (collisions == 0)
         {
-            toActivate.SetActive(onByDefault);
+            doorAnimator.Play("DoorClose");
+
         }
     }
 }
