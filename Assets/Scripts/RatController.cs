b303f00895e59a871254ccb2db038c5f6ecfc6ea
nicolesorial <grace.cola@gmail.com>
2022-11-12 15:10:01 -0500

    he sprint

--- Assets/Scripts/RatController.cs
+++ Assets/Scripts/RatController.cs
@@ -4,24 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 // ReSharper disable SuggestVarOrType_BuiltInTypes
 
 public class RatController : MonoBehaviour
 {
-    [SerializeField]
-    private CharacterController controller;
+    [SerializeField] private CharacterController controller;
 
-    [SerializeField]
-    private Camera mainCamera;
-    
+    [SerializeField] private Camera mainCamera;
+
     private Vector3 velocity;
     private float gravity = -9.8f;
     private float jumpHeightWithoutGravity = 2f;
     public float speed = 5f;
+    public float runSpeed = 10f;
     public float maxDistanceToJump;
-    public bool isHiden;
+    public bool isHidden;
     public bool grounded;
+
     public GameObject vignette;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +31,7 @@ public class RatController : MonoBehaviour
 
     public void WasSpotted()
     {
-        if (!isHiden)
+        if (!isHidden)
         {
             Debug.Log("Rat seen, dead! Back to start point");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -42,12 +44,12 @@ public class RatController : MonoBehaviour
         if (isHiden)
         {
             vignette.SetActive(true);
-            vignette.GetComponent<RawImage>().CrossFadeAlpha(1.0f, 0.3f,false);
+            vignette.GetComponent<RawImage>().CrossFadeAlpha(1.0f, 0.3f, false);
         }
         else
-        {   
+        {
             // vignette.SetActive(false);
-            vignette.GetComponent<RawImage>().CrossFadeAlpha(0.0f, 0.2f,false);
+            vignette.GetComponent<RawImage>().CrossFadeAlpha(0.0f, 0.2f, false);
         }
     }
 
@@ -58,16 +60,26 @@ public class RatController : MonoBehaviour
         Transform cameraTransform = mainCamera.transform;
         //Grounded
         grounded = Physics.Raycast(playerTransform.position, Vector3.down, maxDistanceToJump);
-        
+
         //Movement
         float x = Input.GetAxis("Horizontal");
         // ReSharper disable once SuggestVarOrType_BuiltInTypes
         float z = Input.GetAxis("Vertical");
-        
-        
+
+
         Vector3 movement = (playerTransform.right * x) + (playerTransform.forward * z);
-        controller.Move(movement * (speed * Time.deltaTime));
-        
+        if (movement.magnitude > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(movement * runSpeed * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move(movement * speed * Time.deltaTime);
+            }
+        }
+
         //Rotate alongside the camera
         playerTransform.rotation = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up);
         //Gravity and Jumping
@@ -79,13 +91,13 @@ public class RatController : MonoBehaviour
         {
             velocity.y = 0;
         }
-        
+
         if (Input.GetButtonDown("Jump") && grounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeightWithoutGravity);
         }
+
         controller.Move(velocity * Time.deltaTime);
-        
     }
 
     // Called when a collider is triggered
