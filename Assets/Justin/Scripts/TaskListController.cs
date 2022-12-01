20e02a8df7afb468dccc94f6eb7f63fd8a6b718c
Warspiteful <jalmendral24@gmail.com>
2022-12-01 04:09:26 -0500

    Implemented Collapsable Menu Behavior
    Hide all but active tasks with q button

--- Assets/Justin/Scripts/TaskListController.cs
+++ Assets/Justin/Scripts/TaskListController.cs
@@ -3,32 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CollapsableMenu))]
 public class TaskListController : MonoBehaviour
 {
     [SerializeField] private List<Condition> TaskList;
+    [SerializeField] private List<TaskDisplay> currentDisplays;
+
 
     [SerializeField] private TaskDisplay taskDisplay;
     
     [SerializeField] private Transform parent;
 
+    private CollapsableMenu menu;
 
     private void Start()
     {
+        menu = GetComponent<CollapsableMenu>();
+        currentDisplays = new List<TaskDisplay>();
         UpdateDisplay();
+        menu.ResetPosition();
     }
 
     // https://answers.unity.com/questions/611850/destroy-all-children-of-object.html
     private void UpdateDisplay()
     {
+        currentDisplays.Clear();
         foreach (Transform child in parent)
         {
             Destroy(child.gameObject);
+            
         }
 
 
         foreach (Condition task in TaskList)
         {
-          Instantiate(taskDisplay, parent).SetTask(task);
+            TaskDisplay currDisplay = Instantiate(taskDisplay, parent);
+            currDisplay.SetTask(task);
+            currentDisplays.Add(currDisplay);
+            
         }
     }
 
@@ -37,5 +49,6 @@ public class TaskListController : MonoBehaviour
         
         TaskList.Add(task);
         UpdateDisplay();
+        menu.LoadItems(currentDisplays);
     }
 }
